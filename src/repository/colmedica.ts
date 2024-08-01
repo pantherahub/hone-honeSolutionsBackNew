1f5517f5f496c37778ef1626e3b1d5f323ba7545
brayannPulido <brayavid19@gmail.com>
2024-08-01 14:49:46 -0500

    se sube ajuste en api de creacion de logica

--- src/repository/colmedica.ts
+++ src/repository/colmedica.ts
@@ -114,24 +114,24 @@ export const saveLogicNegotiationTabCupsColmedica = async (
 
     await request.query(insertQuery);
 
-    const selectQuery = `
-      SELECT * 
-      FROM [TB_NegotiationTabCupsColmedica] 
-      WHERE id_NegotiationTabColmedica = @id_NegotiationTabColmedica
-    `;
+    // const selectQuery = `
+    //   SELECT * 
+    //   FROM [TB_NegotiationTabCupsColmedica] 
+    //   WHERE id_NegotiationTabColmedica = @id_NegotiationTabColmedica
+    // `;
 
-    const result = await request.query(selectQuery);
+    // const result = await request.query(selectQuery);
 
-    if (!result.recordset || result.recordset.length === 0) {
-      throw new Error("Failed to retrieve the inserted record");
-    }
+    // if (!result.recordset || result.recordset.length === 0) {
+    //   throw new Error("Failed to retrieve the inserted record");
+    // }
 
-    const insertedRecord = result.recordset[0];
+    // const insertedRecord = result.recordset[0];
 
     return {
       code: 200,
       message: "ok",
-      data: insertedRecord,
+      data: "ok",
     };
   } catch (err: any) {
     console.error("Error in saveLogicNegotiationTabCupsColmedica", err);
