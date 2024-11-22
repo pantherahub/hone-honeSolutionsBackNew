3f0fec20024ff34abe42be5b06c4db079221dc12
David Fernando Torres Zapata <fernando.zapata.live@gmail.com>
2024-11-22 18:26:46 -0500

    add date in logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,8 @@ module.exports = {
 			watch: true, // Opcional, para reiniciar la app cuando los archivos cambien
 			ignore_watch: ['node_modules'],
 			autorestart: false,
+			time: true,
+			log_date_format: "YYYY-MM-DD HH:mm:ss",
 			env: {
 				NODE_ENV: 'development'
 			},
