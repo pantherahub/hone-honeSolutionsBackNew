ef82adb5db85ff13ad7fb330d4eed51103d8cccb
David Fernando Torres Zapata <fernando.zapata.live@gmail.com>
2025-07-18 08:55:21 -0500

    add files in compiled method

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,20 +2,16 @@ module.exports = {
 	apps: [
 		{
 			name: 'prestadores-back',
-			script: 'src/app.ts', // El archivo TypeScript principal
-			interpreter: 'node', // Ejecuta con Node.js
-			interpreter_args: '-r ts-node/register -r tsconfig-paths/register', // Usar ts-node para ejecutar archivos .ts
-			watch: true, // Opcional, para reiniciar la app cuando los archivos cambien
-			ignore_watch: ['node_modules'],
-			autorestart: false,
+			script: 'dist/app.js',
+			interpreter: 'node',
+			node_args: '--max-old-space-size=400',
+			autorestart: true,
+			watch: true,
 			time: true,
-			log_date_format: "YYYY-MM-DD HH:mm:ss",
-			env: {
-				NODE_ENV: 'development'
-			},
-			env_production: {
-				NODE_ENV: 'production'
-			}
+			log_date_format: 'YYYY-MM-DD HH:mm:ss',
+			ignore_watch: ['node_modules', 'src'],
+			env: { NODE_ENV: 'development' },
+			env_production: { NODE_ENV: 'production' }
 		}
 	]
 };
